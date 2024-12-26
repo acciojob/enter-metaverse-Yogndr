270e13fdd2c61c772003651582eee23877b2a511
Yogndr <106022206+Yogndr@users.noreply.github.com>
2024-12-26 20:26:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function Change(){
+	let elem=document.getElementById("status");
+	let content=elem.textcontent;
+
+	let h1element=document.createElement("h1");
+	h1.textcontent=content;
+
+	elem.replaceWith(h1element);
+	
+}
