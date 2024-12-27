a6197120da050630a60ec5158145dd943c835196
Yogndr <106022206+Yogndr@users.noreply.github.com>
2024-12-27 12:32:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //your JS code here. If required.
 function Change(){
-	event.preventDefault();
+	
 	let elem=document.getElementById('status');
 	
 
-	let h1element=document.createElement('h1');
-	h1element.textContent="Entered Metaverse";
+	
+	elem.textContent="Entered Metaverse";
 
-	elem.replaceWith(h1element);
+	// elem.replaceWith(h1element);
 	
 }
